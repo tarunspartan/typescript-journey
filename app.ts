a09a55e82bddbf7bcab07c8e1b8a2572373b2575
Tarun M <tarunspartan1234@outlook.com>
2022-03-27 14:42:22 +0530

    type tuple

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
-const person : {
+const person: {
     name: string;
     age: number;
-    hobbies: string[]
+    hobbies: string[];
+    role: [number, string]
 } = {
     name: 'Tarun',
     age: 24,
-    hobbies: ['Sports','Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [1, 'admin']
 }
 
 console.log(person.name)
 
-for(const hobby of person.hobbies){
+for (const hobby of person.hobbies) {
     console.log(hobby.toLocaleLowerCase())
 }
