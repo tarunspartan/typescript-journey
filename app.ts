dbb3d892c098bc311900d35ec035e2f6006f11e1
Tarun M <tarunspartan1234@outlook.com>
2022-03-29 00:41:52 +0530

    literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
-function combine(i: number | string, j: number | string) {
+function combine(i: number | string, j: number | string, resultConversion: 'as-number' | 'as-text') {
     let result
-    if (typeof i === 'number' && typeof j === 'number') {
-        result = i + j
+    if (typeof i === 'number' && typeof j === 'number' || resultConversion === 'as-number') {
+        result = +i + +j
     } else {
         result = i.toString() + j.toString()
     }
     return result
 }
 
-const combinedAges = combine(30,12)
+const combinedAges = combine(30,12, 'as-number')
 console.log(combinedAges)
 
-const combinedNames = combine('Tarun','M')
+const combinedStringAges = combine('30','12', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Tarun','M', 'as-text')
 console.log(combinedNames)
