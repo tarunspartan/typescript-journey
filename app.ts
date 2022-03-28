9d602478b1ebbf3dbd97a98dd1a4753b78b504cc
Tarun M <tarunspartan1234@outlook.com>
2022-03-29 01:30:56 +0530

    added excludes

--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-let userInput: unknown
-let userName: string
-
-userInput = 5
-userInput = "t"
-
-if (typeof userInput === 'string') {
-    userName = userInput
-}
-
-function generateError(message: string, code: number): never {
-    throw { message, errorCode: code }
-}
-
-generateError('error occured', 500)
+const userName = "T5R7N"
