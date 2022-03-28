e55fffc940202ad3a9077ebbe4790427e0f603f2
Tarun M <tarunspartan1234@outlook.com>
2022-03-29 01:06:59 +0530

    functions

--- app.ts
+++ app.ts
@@ -2,14 +2,23 @@ function add(n1: number, n2: number) {
     return n1 + n2
 }
 
-function printResult(num:number): void {
+function printResult(num: number): void {
     console.log(num)
 }
 
-printResult(add(1,2))
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const r = n1 + n2
+    cb(r)
+}
 
-let combinedValues : (a : number, b: number) => number
+printResult(add(1, 2))
+
+let combinedValues: (a: number, b: number) => number
 
 combinedValues = add
 
-console.log(combinedValues(1,2))
+console.log(combinedValues(1, 2))
+
+addAndHandle(1,2, (r) => {
+    console.log(r)
+})
